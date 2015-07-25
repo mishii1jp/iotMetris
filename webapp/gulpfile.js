2c3068987218866ff25b519c5551aeded43c8506
ishiimao <maoni1jp@gmail.com>
2015-07-25 14:33:24 +0900

    blockrainから必要なものを移植

---
+++ webapp/gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp'),
+fs = require('fs'),
+uglify = require("gulp-uglify"),
+concat = require("gulp-concat"),
+header = require("gulp-header"),
+zip = require("gulp-zip"),
+runSequence = require('run-sequence');
+ 
+var getVersion = function () {
+    info = require("./package.json");
+    return info.version;
+};
+var getCopyright = function () {
+    return fs.readFileSync('Copyright');
+};
+
+gulp.task('js', function () {
+    // Concatenate and Minify JS
+    return gulp.src(['./src/blockrain.jquery.libs.js', './src/blockrain.jquery.src.js', './src/blockrain.jquery.themes.js'])
+    .pipe(concat('blockrain.jquery.js'))
+    .pipe(header(getCopyright(), {version: getVersion()}))
+    .pipe(gulp.dest('./dist'))
+    .pipe(uglify({preserveComments:'none'}))
+    .pipe(concat('blockrain.jquery.min.js'))
+    .pipe(header(getCopyright(), {version: getVersion()}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css', function () {
+    // CSS
+    return gulp.src(['./src/blockrain.css'])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('readme', function () {
+    // Readme
+    return gulp.src(['./README.md'])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('dist', function () {
+    // Create a ZIP File
+    return gulp.src(['./dist/blockrain.jquery.js', './dist/blockrain.jquery.min.js', './dist/blockrain.css'])
+    .pipe(zip('blockrain.zip'))
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('build', function(callback){
+  runSequence('js', 'css', 'readme', 'dist',
+              callback);
+});
+
+gulp.task('default', ['build']);
